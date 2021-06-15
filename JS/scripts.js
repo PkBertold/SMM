6d1236bef3db412eb03c932a1d4294da71118f40
PkBertold <68712243+PkBertold@users.noreply.github.com>
2021-06-15 19:35:54 +0200

    V1.2
    Preparing for "parseint" and testing the string id connections

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,79 @@
 $(document).ready(function(){
+    $("#S").click(function(){ 
+    $(".S").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#E").click(function(){ 
+    $(".E").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#N").click(function(){ 
+    $(".N").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#D").click(function(){ 
+    $(".D").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#M").click(function(){ 
+    $(".M").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#O").click(function(){ 
+    $(".O").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#R").click(function(){ 
+    $(".R").text("0");
+                            });
+});
+
+$(document).ready(function(){
+    $("#Y").click(function(){ 
+    $(".Y").text("0");
+                            });
+});
+
+/*
+$(document).ready(function(){
     $(".S").click(function(){ 
     $("#E").css("background-color", "yellow");
                             });
                             });
+*/
 
+$(document).ready(function(){
+    $("#0").click(function(){ 
+    $(".0").int("1");
+                            });
+});
+
+$(document).ready(function(){
+    $("#1").click(function(){ 
+    $(".1").text("2");
+                            });
+});
+
+$(document).ready(function(){
+    $("#2").click(function(){ 
+    $(".2").text("3");
+                            });
+});
+
+$(document).ready(function(){
+    $("#3").click(function(){ 
+    $(".4").text("4");
+                            });
+});
