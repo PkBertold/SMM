fa93bf9cdff119722623dd26826d2188f7cd81e6
PkBertold <68712243+PkBertold@users.noreply.github.com>
2021-06-17 10:53:44 +0200

    1.2.2
    I might commit suicide...

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,77 +1,47 @@
 $(document).ready(function(){
-    $("#S").click(function(){ 
+    $(".S").click(function(){ 
     $(".S").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#E").click(function(){ 
+    $(".E").click(function(){ 
     $(".E").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#E2").click(function(){ 
-    $(".E").text("0");
-                            });
-});
-
-$(document).ready(function(){
-    $("#E3").click(function(){ 
-    $(".E").text("0");
-                            });
-});
-
-$(document).ready(function(){
-    $("#N").click(function(){ 
+    $(".N").click(function(){ 
     $(".N").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#N2").click(function(){ 
-    $(".N").text("0");
-                            });
-});
-
-$(document).ready(function(){
-    $("#D").click(function(){ 
+    $(".D").click(function(){ 
     $(".D").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#M").click(function(){ 
+    $(".M").click(function(){ 
     $(".M").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#M2").click(function(){ 
-    $(".M").text("0");
-                            });
-});
-
-$(document).ready(function(){
-    $("#O").click(function(){ 
+    $(".O").click(function(){ 
     $(".O").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#O2").click(function(){ 
-    $(".O").text("0");
-                            });
-});
-
-$(document).ready(function(){
-    $("#R").click(function(){ 
+    $(".R").click(function(){ 
     $(".R").text("0");
                             });
 });
 
 $(document).ready(function(){
-    $("#Y").click(function(){ 
+    $(".Y").click(function(){ 
     $(".Y").text("0");
                             });
 });
@@ -110,4 +80,44 @@ $(document).ready(function(){
     $(".4").text("4");
                             });
 });
+
+let x = 0;
+parseInt(x)
+x = Number(String(x).replace(/[^0-9]/g, ''));
+document.getElementById(id).onclick=function(){code}
+
+
+
+$(document).ready(function(){
+    $("#szamlalo").click(function(){ 
+        var number = document.getElementById("#S");
+        var szamlalo = 0;
+            number.innerHTML = szamlalo;
+            number.onclick = function() {
+                szamlalo += 1;          };
+                            });
+});
+
+*/
+let x = 0;
+parseInt(x)
+const add = (function () {
+    let counter = -1;
+    return function () {counter += 1; return counter;}
+})();
+  function szamlalo(){
+    document.getElementById("S").innerHTML = add();
+}
+
+/*
+
+  const add = (function () {
+  let counter = 0;
+  return function () {counter += 1; return counter;}
+})();
+
+function myFunction(){
+  document.getElementById("demo").innerHTML = add();
+}
+
 */
