ad29b80e4e978d1cd41be75aa11219715cc64867
PkBertold <68712243+PkBertold@users.noreply.github.com>
2021-06-17 14:31:55 +0200

    V1.3.2
    Now it is done!!!

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,79 +1,100 @@
-var value = 0;
-parseInt(value)
+var valueS = 0;
+parseInt(valueS)
 
 function addS() {
-    if(value>=9){
-      value = 0;
+    if(valueS>=9){
+      valueS = 0;
     } else {
-    value++;
+    valueS++;
     }
-    document.getElementById("S").innerHTML = value;
+    document.getElementById("S").innerHTML = valueS;
 }
+
+var valueE = 0;
+parseInt(valueE)
 
 function addE() {
-    if(value>=9){
-      value = 0;
+    if(valueE>=9){
+      valueE = 0;
     } else {
-    value++;
+    valueE++;
     }
-    document.getElementById("E").innerHTML = value;
-    document.getElementById("E2").innerHTML = value;
-    document.getElementById("E3").innerHTML = value;
+    document.getElementById("E").innerHTML = valueE;
+    document.getElementById("E2").innerHTML = valueE;
+    document.getElementById("E3").innerHTML = valueE;
 }
+
+var valueN = 0;
+parseInt(valueN)
 
 function addN() {
-    if(value>=9){
-      value = 0;
+    if(valueN>=9){
+      valueN = 0;
     } else {
-    value++;
+    valueN++;
     }
-    document.getElementById("N").innerHTML = value;
-    document.getElementById("N2").innerHTML = value;
+    document.getElementById("N").innerHTML = valueN;
+    document.getElementById("N2").innerHTML = valueN;
 }
+
+var valueD = 0;
+parseInt(valueD)
 
 function addD() {
-    if(value>=9){
-      value = 0;
+    if(valueD>=9){
+      valueD = 0;
     } else {
-    value++;
+    valueD++;
     }
-    document.getElementById("D").innerHTML = value;
+    document.getElementById("D").innerHTML = valueD;
 }
+
+var valueM = 0;
+parseInt(valueM)
 
 function addM() {
-    if(value>=9){
-      value = 0;
+    if(valueM>=9){
+      valueM = 0;
     } else {
-    value++;
+    valueM++;
     }
-    document.getElementById("M").innerHTML = value;
-    document.getElementById("M2").innerHTML = value;
+    document.getElementById("M").innerHTML = valueM;
+    document.getElementById("M2").innerHTML = valueM;
 }
+
+var valueO = 0;
+parseInt(valueO)
 
 function addO() {
-    if(value>=9){
-      value = 0;
+    if(valueO>=9){
+      valueO = 0;
     } else {
-    value++;
+    valueO++;
     }
-    document.getElementById("O").innerHTML = value;
-    document.getElementById("O2").innerHTML = value;
+    document.getElementById("O").innerHTML = valueO;
+    document.getElementById("O2").innerHTML = valueO;
 }
+
+var valueR = 0;
+parseInt(valueR)
 
 function addR() {
-    if(value>=9){
-      value = 0;
+    if(valueR>=9){
+      valueR = 0;
     } else {
-    value++;
+    valueR++;
     }
-    document.getElementById("R").innerHTML = value;
+    document.getElementById("R").innerHTML = valueR;
 }
 
+var valueY = 0;
+parseInt(valueY)
+
 function addY() {
-    if(value>=9){
-      value = 0;
+    if(valueY>=9){
+      valueY = 0;
     } else {
-    value++;
+    valueY++;
     }
-    document.getElementById("Y").innerHTML = value;
+    document.getElementById("Y").innerHTML = valueY;
 }
